438b3d0c8266fd7fd019166c3e19227d57905f6b
LearnMoreTraining <anaravinthh@outlook.com>
2025-08-15 21:13:01 +0530

    added amazon scenario

--- LMTIJuky2025/src/test/java/stepdefinations/AmazonSteps.java
+++ LMTIJuky2025/src/test/java/stepdefinations/AmazonSteps.java
@@ -1,6 +1,23 @@
 package stepdefinations;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class AmazonSteps {
 
+    @Given("user navigates to amazon homepage")
+    public void amazonHomePage(){
 
+    }
+
+    @When("user extract the dropdown values")
+    public void userExtractTheDropdownValues() {
+
+    }
+
+    @Then("validate the dropdown vaues")
+    public void validateTheDropdownVaues() {
+        
+    }
 }
--- LMTIJuky2025/src/test/resources/testdesign/amazon.feature
+++ LMTIJuky2025/src/test/resources/testdesign/amazon.feature
@@ -1,4 +1,10 @@
 Feature: Amazon Home page validation
 
+  @amazon01
+  Scenario: dropdown validation
+    Given user navigates to amazon homepage
+    When user extract the dropdown values
+    Then validate the dropdown vaues
+
 
 
